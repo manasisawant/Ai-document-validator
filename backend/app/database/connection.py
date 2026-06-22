af8a57f1136835f428c29490709ad5db03facdd3
Ne0nR0gUe@9 <mohinipardeshi9425@gmail.com>
2026-06-22 15:31:36 +0530

    UPADTE

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Mohini_2025/20@localhost/ai_document_validator"
+DATABASE_URL = "postgresql://postgres:YOURPASSWORD@localhost/ai_document_validator"
 
 engine = create_engine(DATABASE_URL)
 
